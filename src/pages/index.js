a1296cd3d2abda3aa917843a15626eed023ac12d
Your Vidhatha <vtvidhathatallapally@gmail.com>
2023-12-10 04:36:38 +0530

    added metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Login from '@/components/Login'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  
   return (
+
     <main
       className={`flex flex-col h-screen items-center justify-center p-24 ${inter.className}`}
     >
+          <Head>
+    <title>Arbitrum Wrappd</title>
+</Head>
       <Login/>
        </main>
   )
